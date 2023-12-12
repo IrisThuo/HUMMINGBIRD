6ff44885df7789a9487bbd08c60a3083e63bd055
Iris <iris.thuo@student.moringaschool.com>
2023-12-12 15:01:00 +0300

    creatingbase  components

---
+++ src/Components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './Footer';
+
+
+function Hero(props) {
+    return (
+        <div>
+        <Footer/>
+        </div>
+
+
+    );
+}
+
+export default Hero;
