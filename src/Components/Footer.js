3ebc790375a6a6e698a689dd6f79348756884b8d
Iris <iris.thuo@student.moringaschool.com>
2024-01-02 18:41:51 +0300

    Footer links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,9 +2,43 @@
 
  function Footer(props) {
     return (
-     <Footer>
-     
-     </Footer>
+     <div className='footer'>
+      <div className='sb_footer_section_padding'>
+        <div className='sb_footer-links'>
+
+  {/*part a of footer links */}
+          <div className='sb_footer-links-div'>
+            <h4>For Business</h4>
+            <a href=''>
+             <p>Contact</p>
+            </a>
+            <a href=''>
+            <p>Email</p>
+            </a>
+            <a href=''>
+            <p>Address</p>
+            </a>
+
+  {/* part b of footer links*/}
+        <div className='sb_Footer-inks_div'>
+        <h4>Resources and Partners</h4>
+        <a href=''>
+        <p>Partner A</p>
+       </a>
+       <a href=''>
+       <p>Partner B</p>
+      </a>
+      <a href=''>
+      <p>Partner c</p>
+     </a>
+        </div>
+
+
+          </div>
+        </div>
+      </div>
+     </div>
+
     );
  }
 
