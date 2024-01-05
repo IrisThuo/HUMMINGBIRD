01b73c901dca84d494e6fa30c789f50f313f7f73
Iris <iris.thuo@student.moringaschool.com>
2024-01-05 22:55:49 +0300

    Hero page PART 2

--- src/Views/Landing/Hero.js
+++ src/Views/Landing/Hero.js
@@ -1,30 +1,40 @@
 import React from "react";
-import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import "/home/name/HUMMINGBIRD/hummingbird/src/Views/Landing/Hero.css"
 import HeroPic from "/home/name/HUMMINGBIRD/hummingbird/src/assets/HeroPic.png";
-
+import ballot from "/home/name/HUMMINGBIRD/hummingbird/src/assets/ballot.png";
+import Navbar from "../../Components/Navbar/Navbar";
 
 function Hero(props) {
   return (
     <div>
+   
     <Navbar/>
 
+
+
     <div className="story-btn">
-        <a href="/stories">View Stories</a>
+        <img className="vote" src={ballot} alt=""/>
+        <a href="/stories">Vote for your favourite story</a>
     </div>
-    
+
     <div className="Hero_section">
        <div className="Hero_section_box">
           <div className="Hero_section_box_right">
-           <div class = "vertical"></div>
-            <h1><a href="/about">HOW WE GOT STARTED</a></h1>
+
+          <div class = "vertical">
+           <h1><a href="/about">HOW WE GOT STARTED</a></h1>
+           </div>
+           <br></br>
+            <div>
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum suscipit magna a semper pulvinar. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
             </p>
+            </div>
             <div className="Join-btn">
               <button>Join us</button>
             </div>
+
           </div>
           <div className="Hero_section_box_left">
           <div className="planter1">
