4547d2e67d78cff4072435874a702ec9c2c2490c
Iris <iris.thuo@student.moringaschool.com>
2024-07-31 15:32:35 +0300

    james bio 2 removed

--- src/Components/Members/Members.js
+++ src/Components/Members/Members.js
@@ -52,7 +52,6 @@ function Members(props) {
 
           <SwiperSlide>
           <Card
-          
           name='James Kamau'
           desc= 'My name is James Kamau aka chief hummingbird. I am a conservation biologist by profession and currently a masters student taking plant ecology in Kenyatta university. I am a community leader where I help my community by guiding them to what they need as well as making their environment better through very active participation in tree growing, education, training and stewardship
           I am the chairman and founder of the Millennial Environmentalists a registered society in Kenya Our main pillars are advocacy and capacity building We look to empower anyone interested in conservation to do the work in the simplest cheapest and most sustainable ways accessible to them in their immediate environment. In our short existence we have pioneered projects that are self-sustaining that continue to shape the climate action narrative in the grass root of Kenya. This projects aim to create individuals who are empowered to handle climate change in the own niche as well as benefit from the projects.
@@ -74,17 +73,6 @@ function Members(props) {
                 name='Eric Mwangi'
                 desc= 'He is an ambitious and passionate environmental activist engaged in environmental preservation and reclamation in various sectors. He is a graduate with a bachelors degree in Physical Science from Meru University of Science and Technology and is currently an Alumni providing mentorship to the current students in the environmental field. He is a practicing hydrologist and projects coordinator at inner core geoconsultancy firm with years of experience in the field and has worked with major industry stake holders in shaping policies in the water sector. He is also a philanthropist and is currently a junior director in Jamii Outreach, a community based organization in Kibra working with less privileged children and youth'/>
                 </SwiperSlide>
-
-
-                <SwiperSlide>
-                <Card
-                image={Eric}
-                name='James Kamau'
-                desc= 'My name is James Kamau aka chief hummingbird. I am a conservation biologist by profession and currently a masters student taking plant ecology in Kenyatta university. I am a community leader where I help my community by guiding them to what they need as well as making their environment better through very active participation in tree growing, education, training and stewardship
-                I am the chairman and founder of the Millennial Environmentalists a registered society in Kenya Our main pillars are advocacy and capacity building We look to empower anyone interested in conservation to do the work in the simplest cheapest and most sustainable ways accessible to them in their immediate environment. In our short existence we have pioneered projects that are self-sustaining that continue to shape the climate action narrative in the grass root of Kenya. This projects aim to create individuals who are empowered to handle climate change in the own niche as well as benefit from the projects.
-                I am a planet steward that is here to elevate the relationship between man and environment. I am a student of nature and history and I believe for our history to change we must change now. I believe that the journey matters as much as the end result but most importantly the company in your journey.'/>
-                </SwiperSlide>
-
 
           </Swiper>
             </div>
